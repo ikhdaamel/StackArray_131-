2bd5c84294e912380b75c1e9c4cc7873af56e7a2
ikhdaamel <ikhda.new45@gmail.com>
2023-06-15 15:27:21 +0700

    step 3

--- stackarray_131/stackarray_131.cpp
+++ stackarray_131/stackarray_131.cpp
@@ -18,4 +18,11 @@ public:
 		}
 
 		top++;																//step 2
+		stack_array[top] = element;											//step 3
+		cout << endl;
+		cout << element << "ditambahkan (pushed)" << endl;
+
+		return element;
+	}
+
 	
